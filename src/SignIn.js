bc21d60a2abbb128dd37398674e39a15b3f06b45
lfaucon <lpfaucon@gmail.com>
2019-12-18 19:41:02 +0100

    Remove login with FB

--- src/SignIn.js
+++ src/SignIn.js
@@ -16,7 +16,6 @@ class SignIn extends React.Component {
       // We will display Google and Facebook as auth providers.
       signInOptions: [
         firebase.auth.GoogleAuthProvider.PROVIDER_ID,
-        firebase.auth.FacebookAuthProvider.PROVIDER_ID,
         firebase.auth.EmailAuthProvider.PROVIDER_ID,
         firebase.auth.PhoneAuthProvider.PROVIDER_ID,
         firebaseui.auth.AnonymousAuthProvider.PROVIDER_ID
