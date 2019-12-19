2cef2ffecd5f41ab9f6f518ce1840afb93b6c1e7
antoinexp <antoinexp@gmail.com>
2019-12-19 13:37:39 +0100

    Transition

--- src/App.js
+++ src/App.js
@@ -8,46 +8,57 @@ import Settings from "./Settings";
 import SignIn from "./SignIn";
 import FetchQuiz from "./FetchQuiz";
 import MyQuizzes from "./MyQuizzes";
-import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
+import { BrowserRouter as Router, Route } from "react-router-dom";
+import { CSSTransition } from "react-transition-group";
+import "./transitions.css";
 
 const App = () => {
   const path = window.location.pathname;
   const [quiz, setQuiz] = useState(null);
   const [loggedIn, setLoggedIn] = useState(false);
 
+  const routes = [
+    { path: "/", Component: <Home /> },
+    { path: "/Home", Component: <Home /> },
+    {
+      path: "/quizList",
+      Component: <QuizList setQuiz={setQuiz} />
+    },
+    {
+      path: "/quiz",
+      Component: <Quiz quiz={quiz} setQuiz={setQuiz} />
+    },
+    { path: "/editor", Component: <Editor quiz={quiz} /> },
+    { path: "/stats", Component: <Stats /> },
+    {
+      path: "/settings",
+      Component: <Settings setLoggedIn={setLoggedIn} />
+    },
+    {
+      path: "/q/:quizId",
+      Component: <FetchQuiz setQuiz={setQuiz} path={path} />
+    },
+    { path: "/myquizzes", Component: <MyQuizzes /> }
+  ];
+
   if (loggedIn) {
     return (
       <div className="App">
         <Router>
-          <Switch>
-            <Route exact path="/">
-              <Home />
+          {routes.map(({ path, Component }) => (
+            <Route key={path} exact path={path}>
+              {({ match }) => (
+                <CSSTransition
+                  in={match != null}
+                  timeout={300}
+                  classNames="page"
+                  unmountOnExit
+                >
+                  {Component}
+                </CSSTransition>
+              )}
             </Route>
-            <Route exact path="/home">
-              <Home />
-            </Route>
-            <Route exact path="/quizList">
-              <QuizList setQuiz={setQuiz} />
-            </Route>
-            <Route exact path="/quiz">
-              <Quiz quiz={quiz} setQuiz={setQuiz} />
-            </Route>
-            <Route exact path="/editor">
-              <Editor quiz={quiz} />
-            </Route>
-            <Route exact path="/stats">
-              <Stats />
-            </Route>
-            <Route exact path="/settings">
-              <Settings setLoggedIn={setLoggedIn} />
-            </Route>
-            <Route exact path="/q/:quizId">
-              <FetchQuiz setQuiz={setQuiz} path={path} />
-            </Route>
-            <Route exact path="/myquizzes">
-              <MyQuizzes />
-            </Route>
-          </Switch>
+          ))}
         </Router>
       </div>
     );
