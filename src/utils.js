00adc3ddf1529494b1fed5829bcea2da6a2d241e
Louis Faucon <lpfaucon@gmail.com>
2020-01-21 08:45:35 +0100

    Remove some dark colors and add a few more (#16)

--- src/utils.js
+++ src/utils.js
@@ -2,9 +2,7 @@ const colors = [
   "orange",
   "orangered",
   "yellowgreen",
-  "purple",
   "blueviolet",
-  "brown",
   "cadetblue",
   "chocolate",
   "coral",
@@ -12,16 +10,32 @@ const colors = [
   "crimson",
   "darkcyan",
   "darkorange",
-  "darkmagenta",
   "darksalmon",
   "deeppink",
-  "firebrick"
-].sort(() => 0.5 - Math.random());
+  "palegreen",
+  "paleturquoise",
+  "palevioletred",
+  "powderblue",
+  "pink",
+  "sandybrown",
+  "skyblue",
+  "springgreen",
+  "steelblue",
+  "tan",
+  "thistle",
+  "tomato",
+  "turquoise",
+  "violet",
+  "wheat",
+  "yellow",
+  "yellowgreen"
+];
 
-var idx = 0;
-
+var idx = -1
 export const getColor = () => {
-  idx = (idx + 1) % colors.length;
+  const newIdx = Math.floor(Math.random() * colors.length);
+  if (newIdx === idx) return getColor();
+  idx = newIdx
   return colors[idx];
 };
 
@@ -31,75 +45,6 @@ export const httpGet = url => {
   xmlHttp.send(null);
   return xmlHttp.responseText;
 };
-
-export const quizList = [
-  {
-    name: "Bayes Rule",
-    questions: [
-      ["La crédence des théorie est la crédence des données", "Faux", "Vrai"],
-      [
-        "Un muslman tiré au hazard ressemble plus à un chinois qu'à un arabe",
-        "Vrai",
-        "Faux"
-      ],
-      ["Le prénom de Bayes était", "Thomas", "Pierre", "Simon", "Ray"],
-      [
-        "Si un couple tiré au hazard a deux enfants dont au moins un garçon. Quelle la probabilité qu'ils ont aussi une fille?",
-        "2/3",
-        "1/3",
-        "1/2"
-      ]
-    ]
-  },
-  {
-    name: "Turing and his Machines",
-    questions: [
-      ["Alan Turing was born in...", "1912", "1900", "1924", "1936"],
-      [
-        "The first recipient of the Turing award was ...",
-        "Alan Perlis",
-        "Alan Turing",
-        "Ada Lovelace",
-        "Donald Knuth"
-      ],
-      [
-        "During the second world war, Alan Turing helped breaking the code of ...",
-        "The Enigma machine",
-        "The Turing machine",
-        "The Crypto machine",
-        "The Cypher machine"
-      ],
-      ["Turing machines have an infinite number of states", "False", "True"],
-      ["Turing machines are deterministic", "True", "False"],
-      [
-        "There exists an infinite number of possible Turing machines",
-        "True",
-        "False"
-      ],
-      ["Turing machines read and write on an infinite tape", "True", "False"],
-      ["Turing machines can compute any sequence of number", "False", "True"],
-      [
-        "Turing machines can compute everything that can be computed using a usual programming language",
-        "True",
-        "False"
-      ]
-    ]
-  },
-  {
-    name: "Multiplications",
-    questions: [
-      ["6 * 7 = ?", "42", "46", "36"],
-      ["142857 * 7 = ?", "999999", "1000009", "998899"],
-      ["11 * 13 * 19 * 52579 = 142857143", "True", "False"],
-      [
-        "777777777 * 142857143 = ?",
-        "111111111111111111",
-        "11111111111111111",
-        "1111111111111111111"
-      ]
-    ]
-  }
-];
 
 export const shortID = n => {
   const alphabet =
